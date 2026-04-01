a55070a518ad30bb971951f4816912309f8bf810
Benedikt <bfj.koehn@gmail.com>
2026-04-01 12:08:13 +0200

    Allowed const volatile register spaces to omit initialization

--- lib/Dialect/CoreDSL/CoreDSLOps.cpp
+++ lib/Dialect/CoreDSL/CoreDSLOps.cpp
@@ -453,7 +453,10 @@ LogicalResult RegisterOp::verify() {
     if (init.getElementType() != getRegType())
       return emitError("initial value type must match the register type");
   } else {
-    if (getIsConst())
+    // Do not error for const volatile register spaces, as in this case the
+    // value may be changed arbitrarily by an external entity. Thus, the
+    // assigned value may well never be read at all
+    if (getIsConst() && !getIsVolatile())
       return emitError("Const registers must be initialized");
   }
 
