7a4e92702a8586f02fffa15f46b8ac1a9cdc1a94
Benedikt <bfj.koehn@gmail.com>
2026-04-27 14:22:20 +0200

    Moved dialects needed for CoreDSL to dependentDialects in TableGen. This should make mlir load all dialects that are needed automatically when the CoreDSL dialect is loaded

--- tools/shortnail-opt/shortnail-opt.cpp
+++ tools/shortnail-opt/shortnail-opt.cpp
@@ -9,27 +9,15 @@
 #include "shortnail/Conversion/Passes.h"
 #include "shortnail/Dialect/CoreDSL/CoreDSLDialect.h"
 
-#include "circt/InitAllDialects.h"
-#include "circt/InitAllPasses.h"
-
-#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/Extensions/InlinerExtension.h"
-#include "mlir/Dialect/Func/IR/FuncOps.h"
-#include "mlir/Dialect/MemRef/IR/MemRef.h"
-#include "mlir/IR/Dialect.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 #include "mlir/Transforms/Passes.h"
 
 using namespace mlir;
-using namespace circt;
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   registry.insert<coredsl::CoreDSLDialect>();
-  registry.insert<comb::CombDialect, hw::HWDialect, seq::SeqDialect,
-                  hwarith::HWArithDialect, sv::SVDialect>();
-  registry.insert<arith::ArithDialect, memref::MemRefDialect, func::FuncDialect,
-                  scf::SCFDialect>();
 
   mlir::func::registerInlinerExtension(registry);
   mlir::registerCSEPass();
