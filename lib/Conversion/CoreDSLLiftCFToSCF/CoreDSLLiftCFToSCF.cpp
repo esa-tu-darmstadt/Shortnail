bfc78804ea194269035f12ca974f39b063193699
Benedikt <bfj.koehn@gmail.com>
2026-05-04 15:21:46 +0200

    Moved converting regions out into a template function and added func operations as well

--- lib/Conversion/CoreDSLLiftCFToSCF/CoreDSLLiftCFToSCF.cpp
+++ lib/Conversion/CoreDSLLiftCFToSCF/CoreDSLLiftCFToSCF.cpp
@@ -2,6 +2,7 @@
 #include "shortnail/Dialect/CoreDSL/CoreDSLOps.h"
 
 #include "mlir/Conversion/ControlFlowToSCF/ControlFlowToSCF.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 
 namespace mlir {
 namespace shortnail {
@@ -15,48 +16,43 @@ using namespace mlir::shortnail;
 
 namespace {
 
+template <typename OpType>
+WalkResult convertRegionToSCF(OpType op) {
+  ControlFlowToSCFTransformation transformation;
+  DominanceInfo dominanceInfo;
+  LogicalResult res = transformCFGToSCF(op.getRegion(), transformation, dominanceInfo);
+  if (res.failed()) {
+    op->emitError("Failed to convert all cf ops to scf");
+    return WalkResult::interrupt();
+  }
+  return WalkResult::skip();
+}
+
 struct CoreDSLLiftCFToSCF
     : public mlir::shortnail::impl::CoreDSLLiftCFToSCFBase<CoreDSLLiftCFToSCF> {
   using CoreDSLLiftCFToSCFBase::CoreDSLLiftCFToSCFBase;
 
   void runOnOperation() override {
     auto isaxOp = getOperation();
+    // TODO: does this work for cf ops that are in deeper nesting?
     // We can't use the LiftControlFlowToSCF pass, because it only visits func
     // operations
-    auto res =
-        isaxOp->walk<WalkOrder::PreOrder>([&](coredsl::InstructionOp instr) {
-          ControlFlowToSCFTransformation transformation;
-          DominanceInfo dominance_info;
-          LogicalResult res = transformCFGToSCF(instr.getRegion(),
-                                                transformation, dominance_info);
-          if (res.failed()) {
-            instr->emitError("Failed to convert all cf ops to scf");
-            return WalkResult::interrupt();
-          }
-          return WalkResult::skip();
-        });
-    res = isaxOp->walk<WalkOrder::PreOrder>([&](coredsl::AlwaysOp always) {
-      ControlFlowToSCFTransformation transformation;
-      DominanceInfo dominance_info;
-      LogicalResult res =
-          transformCFGToSCF(always.getRegion(), transformation, dominance_info);
-      if (res.failed()) {
-        always->emitError("Failed to convert all cf ops to scf");
-        return WalkResult::interrupt();
-      }
-      return WalkResult::skip();
-    });
-    res = isaxOp->walk<WalkOrder::PostOrder>([&](coredsl::SpawnOp spawn) {
-      ControlFlowToSCFTransformation transformation;
-      DominanceInfo dominance_info;
-      LogicalResult res =
-          transformCFGToSCF(spawn.getRegion(), transformation, dominance_info);
-      if (res.failed()) {
-        spawn->emitError("Failed to convert all cf ops to scf");
-        return WalkResult::interrupt();
-      }
-      return WalkResult::skip();
-    });
+    auto res = isaxOp->walk<WalkOrder::PreOrder>(convertRegionToSCF<coredsl::InstructionOp>);
+    if (res == WalkResult::interrupt()) {
+      return signalPassFailure();
+    }
+    res = isaxOp->walk<WalkOrder::PreOrder>(convertRegionToSCF<coredsl::AlwaysOp>);
+    if (res == WalkResult::interrupt()) {
+      return signalPassFailure();
+    }
+    res = isaxOp->walk<WalkOrder::PreOrder>(convertRegionToSCF<coredsl::SpawnOp>);
+    if (res == WalkResult::interrupt()) {
+      return signalPassFailure();
+    }
+    res = isaxOp->walk<WalkOrder::PreOrder>(convertRegionToSCF<func::FuncOp>);
+    if (res == WalkResult::interrupt()) {
+      return signalPassFailure();
+    }
   }
 };
 
