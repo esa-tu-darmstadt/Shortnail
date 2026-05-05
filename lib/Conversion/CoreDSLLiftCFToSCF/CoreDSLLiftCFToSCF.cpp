b5811d3c252317ae17ecc07388290e585ac87529
Benedikt <bfj.koehn@gmail.com>
2026-05-05 16:17:43 +0200

    Added conversion of scf::ExecuteRegionOp as well and properly got the dominanceInfo. Changed iteration order to preorder

--- lib/Conversion/CoreDSLLiftCFToSCF/CoreDSLLiftCFToSCF.cpp
+++ lib/Conversion/CoreDSLLiftCFToSCF/CoreDSLLiftCFToSCF.cpp
@@ -3,6 +3,7 @@
 
 #include "mlir/Conversion/ControlFlowToSCF/ControlFlowToSCF.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
 
 namespace mlir {
 namespace shortnail {
@@ -16,42 +17,63 @@ using namespace mlir::shortnail;
 
 namespace {
 
-template <typename OpType>
-WalkResult convertRegionToSCF(OpType op) {
-  ControlFlowToSCFTransformation transformation;
-  DominanceInfo dominanceInfo;
-  LogicalResult res = transformCFGToSCF(op.getRegion(), transformation, dominanceInfo);
-  if (res.failed()) {
-    op->emitError("Failed to convert all cf ops to scf");
-    return WalkResult::interrupt();
-  }
-  return WalkResult::skip();
-}
-
 struct CoreDSLLiftCFToSCF
     : public mlir::shortnail::impl::CoreDSLLiftCFToSCFBase<CoreDSLLiftCFToSCF> {
   using CoreDSLLiftCFToSCFBase::CoreDSLLiftCFToSCFBase;
 
+  // This needs to be a class rather than a lambda because we cannot have
+  // templated lambdas and this needs to be explicitly instantiated with
+  // multiple op types for the walk method
+  template <typename OpType>
+  struct RegionToSCFConverter {
+    // So we can use Pass::getChildAnalysis
+    friend struct CoreDSLLiftCFToSCF;
+    CoreDSLLiftCFToSCF &pass;
+    bool &changed;
+
+    WalkResult operator()(OpType op) {
+      ControlFlowToSCFTransformation transformation;
+      DominanceInfo &dominanceInfo = pass.getChildAnalysis<DominanceInfo>(op);
+      FailureOr<bool> res = transformCFGToSCF(op.getRegion(), transformation, dominanceInfo);
+      if (failed(res)) {
+        op->emitError("Failed to convert all cf ops to scf");
+        return WalkResult::interrupt();
+      }
+      changed |= *res;
+      return WalkResult::skip();
+    }
+  };
+
   void runOnOperation() override {
     auto isaxOp = getOperation();
-    // TODO: does this work for cf ops that are in deeper nesting?
-    // We can't use the LiftControlFlowToSCF pass, because it only visits func
-    // operations
-    auto res = isaxOp->walk<WalkOrder::PreOrder>(convertRegionToSCF<coredsl::InstructionOp>);
+    // Visit all operations that can have multiple child blocks. As SCF ops
+    // other than scf::ExecuteRegionOp cannot have multiple child blocks, they
+    // don't need to be visited here
+    bool changed = false;
+    static constexpr WalkOrder ORDER = WalkOrder::PostOrder;
+    auto res = isaxOp->walk<ORDER>(RegionToSCFConverter<coredsl::InstructionOp>{*this, changed});
     if (res == WalkResult::interrupt()) {
       return signalPassFailure();
     }
-    res = isaxOp->walk<WalkOrder::PreOrder>(convertRegionToSCF<coredsl::AlwaysOp>);
+    res = isaxOp->walk<ORDER>(RegionToSCFConverter<coredsl::AlwaysOp>{*this, changed});
     if (res == WalkResult::interrupt()) {
       return signalPassFailure();
     }
-    res = isaxOp->walk<WalkOrder::PreOrder>(convertRegionToSCF<coredsl::SpawnOp>);
+    res = isaxOp->walk<ORDER>(RegionToSCFConverter<coredsl::SpawnOp>{*this, changed});
     if (res == WalkResult::interrupt()) {
       return signalPassFailure();
     }
-    res = isaxOp->walk<WalkOrder::PreOrder>(convertRegionToSCF<func::FuncOp>);
+    res = isaxOp->walk<ORDER>(RegionToSCFConverter<func::FuncOp>{*this, changed});
     if (res == WalkResult::interrupt()) {
       return signalPassFailure();
+    }
+    // TODO: this will always be a child of the other ops, so we could just do this recursively in the callback
+    res = isaxOp->walk<ORDER>(RegionToSCFConverter<scf::ExecuteRegionOp>{*this, changed});
+    if (res == WalkResult::interrupt()) {
+      return signalPassFailure();
+    }
+    if (!changed) {
+      markAllAnalysesPreserved();
     }
   }
 };
